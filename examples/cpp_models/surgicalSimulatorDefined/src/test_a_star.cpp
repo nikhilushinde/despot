3374c905acd0b2c6b2df7cfa5ccfcbc1a8b610f9
nikhilushinde <nshinde@eng.ucsd.edu>
2020-10-16 21:27:43 -0700

    changed the a star test file so that there are more verbose outputs

--- examples/cpp_models/surgicalSimulatorDefined/src/test_a_star.cpp
+++ examples/cpp_models/surgicalSimulatorDefined/src/test_a_star.cpp
@@ -27,6 +27,13 @@ int main() {
     ACT_TYPE current_action;
     bool error; 
     float cost;
+    double all_values_along_path[actions_to_goal.size()];
+
+    cout << "STARTING STATE: " << endl;
+    test_environment.printState();
+    cout << endl;
+
+    cout << "Printing all values along path: ";
     for (int i = 0; i < actions_to_goal.size(); i++) {
         // step the environment 
         current_action = actions_to_goal[i];
@@ -34,13 +41,70 @@ int main() {
         test_environment.step(action_array, error, cost);
         // render the environment
         Renderer.render_environment(test_environment, 50);
+
+        all_values_along_path[i] = planner.get_state_cost(test_environment);
+        if (current_action == 0) {
+			cout << "xRight: ";
+		} else if (current_action == 1) {
+			cout << "xLeft: ";
+		} else if (current_action == 2) {
+			cout << "yUp: ";
+		} else if (current_action == 3) {
+			cout << "yDown: ";
+		} else if (current_action == 4) {
+			cout << "thetaUp: ";
+		} else if (current_action == 5) {
+			cout << "thetaDown: ";
+		}
+        cout << all_values_along_path[i] << ", ";
     }   
+    cout << endl << endl;
 
     cout << "get NON discounted goal cost: " << planner.get_goal_cost() << endl;
     cout << "get discounted value: " << planner.get_discounted_goal_value(actions_to_goal) << endl;
     cout << "the terminal reward alone is: " << TERMINAL_REWARD_g << endl;
     cout << "the discount factor is: " << Globals::Discount(1) << endl;
     cout << "environment is at goal: " << planner.is_goal(test_environment) << endl;
+    cout << endl << endl << endl;
+
+    environment env;
+    // get the values of all the surrounding states
+    for (int action = 0; action < 6; action++) {
+        // get the surrounding state and print everything about it 
+        cout << "SURROUNDING STATE AFTER ACTION: ";
+        if (action == 0) {
+			cout << "xRight: ";
+		} else if (action == 1) {
+			cout << "xLeft: ";
+		} else if (action == 2) {
+			cout << "yUp: ";
+		} else if (action == 3) {
+			cout << "yDown: ";
+		} else if (action == 4) {
+			cout << "thetaUp: ";
+		} else if (action == 5) {
+			cout << "thetaDown: ";
+		}
+        cout << endl;
+
+        environment child_env = env;
+        int_to_action_array_g(action, action_array, NUM_ROBOT_ARMS_g);
+        child_env.step(action_array, error, cost);
+
+        child_env.robObj_m.printState();
+        cout << endl;
+        planner.plan_a_star(child_env, false);
+        actions_to_goal.clear();
+        planner.get_path(actions_to_goal);
+
+        cout << "get NON discounted goal cost: " << planner.get_goal_cost() << endl;
+        cout << "get discounted value: " << planner.get_discounted_goal_value(actions_to_goal) << endl;
+        cout << "the terminal reward alone is: " << TERMINAL_REWARD_g << endl;
+
+        cout << endl << endl << endl;
+        
+    }
+
 
     return 0;
 }
